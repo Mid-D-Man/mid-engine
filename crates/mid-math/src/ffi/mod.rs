0b8f85b63aa9979e1a6ab7bc187b5e6d8d861741
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-05-03 11:34:27 +0100

    Update mod.rs

--- crates/mid-math/src/ffi/mod.rs
+++ crates/mid-math/src/ffi/mod.rs
@@ -3,4 +3,9 @@
 pub mod types;
 pub mod exports;
 
-pub use types::{CAffine3, CMat3, CMat4, CQuat, CVec2, CVec3, CVec4};
+pub use types::{
+    // f32
+    CAffine3, CMat3, CMat4, CQuat, CVec2, CVec3, CVec4,
+    // f64
+    CDAffine3, CDMat2, CDMat3, CDMat4, CDQuat, CDVec2, CDVec3, CDVec4,
+};
