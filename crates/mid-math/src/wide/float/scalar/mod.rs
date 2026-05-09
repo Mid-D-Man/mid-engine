c0f96beedcea2089b6b6029aaa4c181b7d4a5fc5
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-05-09 19:28:32 +0100

    Update mod.rs

--- crates/mid-math/src/wide/float/scalar/mod.rs
+++ crates/mid-math/src/wide/float/scalar/mod.rs
@@ -1,9 +1,12 @@
 // crates/mid-math/src/wide/float/scalar/mod.rs
+// crates/mid-math/src/wide/float/scalar/mod.rs
 pub mod mask4;
 pub mod f32x4;
 pub mod vec3x4;
+pub mod quatx4;
 
 pub use mask4::Mask4;
 #[allow(non_camel_case_types)]
 pub use f32x4::f32x4;
 pub use vec3x4::Vec3x4;
+pub use quatx4::QuatX4;
