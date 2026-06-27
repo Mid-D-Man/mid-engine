2b5e0b7595918f73539d7289642fe46b769abea8
AbdulHamid Mamman Suleiman <94022993+Mid-D-Man@users.noreply.github.com>
2026-06-27 18:39:47 +0100

    Update vec3.rs

--- crates/mid-math/src/f32/wasm/vec3.rs
+++ crates/mid-math/src/f32/wasm/vec3.rs
@@ -34,6 +34,7 @@ impl Vec3 {
     pub const NEG_X: Self = unsafe { UnionCast { f: [-1.0,  0.0,  0.0, 0.0] }.v };
     pub const NEG_Y: Self = unsafe { UnionCast { f: [ 0.0, -1.0,  0.0, 0.0] }.v };
     pub const NEG_Z: Self = unsafe { UnionCast { f: [ 0.0,  0.0, -1.0, 0.0] }.v };
+    pub const NAN:   Self = unsafe { UnionCast { f: [f32::NAN, f32::NAN, f32::NAN, f32::NAN] }.v };
 
     #[inline(always)]
     pub fn new(x: f32, y: f32, z: f32) -> Self {
@@ -124,6 +125,11 @@ impl Vec3 {
     }
     #[inline] pub fn approx_eq(self, rhs: Self) -> bool {
         (self - rhs).abs().length_sq() < EPSILON * EPSILON
+    }
+    #[inline] pub fn abs_diff_eq(self, rhs: Self, max_abs_diff: f32) -> bool {
+        (self.x - rhs.x).abs() < max_abs_diff
+            && (self.y - rhs.y).abs() < max_abs_diff
+            && (self.z - rhs.z).abs() < max_abs_diff
     }
     #[inline] pub fn approx_eq_eps(self, rhs: Self, eps: f32) -> bool {
         (self.x - rhs.x).abs() < eps
